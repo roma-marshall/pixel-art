4cf4d16d558be84240d302295060920e7009be41
roma_marshall <roma_marshall@outlook.com>
2022-07-01 14:17:45 +0200

    selected color added

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ for (let i = 0; i < color.length; i++) {
   color[i].addEventListener("click", function getColor() {
     let color = this.getAttribute("class");
     console.log(color);
+    document.querySelector("div").classList = color;
   });
 }
