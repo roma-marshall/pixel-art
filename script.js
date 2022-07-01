8f2c29b39301ac9473c97078767d9af79f9fb9d8
roma_marshall <roma_marshall@outlook.com>
2022-07-01 14:19:16 +0200

    table draw added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let color = document.querySelectorAll("li");
+let card = document.querySelectorAll("td");
 
 // getColor
 for (let i = 0; i < color.length; i++) {
@@ -6,5 +7,13 @@ for (let i = 0; i < color.length; i++) {
     let color = this.getAttribute("class");
     console.log(color);
     document.querySelector("div").classList = color;
+  });
+}
+
+// drawFunc
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener("click", function drawFunc() {
+    let color = document.querySelector("div").className;
+    this.classList = color;
   });
 }
