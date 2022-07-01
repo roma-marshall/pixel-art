b1e7fc4fa7228e6083eccb9b4f6467c350eb4f9b
roma_marshall <roma_marshall@outlook.com>
2022-07-01 14:33:00 +0200

    generate tables inner html

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let color = document.querySelectorAll("li");
-let card = document.querySelectorAll("td");
+// generate tables row/cell
+let table = document.querySelector("table");
+for (let i = 0; i < 8; i++) {
+  let row = table.insertRow(i);
+  for (let j = 0; j < 8; j++) {
+    let cell = row.insertCell(j);
+  }
+}
 
 // getColor
+let color = document.querySelectorAll("li");
 for (let i = 0; i < color.length; i++) {
   color[i].addEventListener("click", function getColor() {
     let color = this.getAttribute("class");
@@ -11,6 +18,7 @@ for (let i = 0; i < color.length; i++) {
 }
 
 // drawFunc
+let card = document.querySelectorAll("td");
 for (let i = 0; i < card.length; i++) {
   card[i].addEventListener("click", function drawFunc() {
     let color = document.querySelector("div").className;
