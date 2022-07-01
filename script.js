58e0f716a7052b65b7e5345212f9d76682efcc30
roma_marshall <roma_marshall@outlook.com>
2022-07-01 14:14:06 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let color = document.querySelectorAll("li");
+
+// getColor
+for (let i = 0; i < color.length; i++) {
+  color[i].addEventListener("click", function getColor() {
+    let color = this.getAttribute("class");
+    console.log(color);
+  });
+}
